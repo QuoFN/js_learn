2781202a8f85139595aa25d95f17113538639244
QuoFN <116729072+QuoFN@users.noreply.github.com>
2026-02-03 16:45:14 +0300

    Update index.js

--- Learn/js/index.js
+++ Learn/js/index.js
@@ -1,95 +1,100 @@
 
 // ---------------------------------------------------------------------------------------------------------------
 
-            //   First
+              // First
 
-// const number = prompt('Введите число');
-// const fixNumber = 2;
+const number = prompt('Введите число');
+const fixNumber = 2;
 
-// if (number % fixNumber) {
-//     alert('Число не четное');
-// } else {
-//     alert('Число четное');
-// }
+if (number % fixNumber) {
+    alert('Число не четное');
+} else {
+    alert('Число четное');
+}
 
 // ------------------------------------
 
-//              Second
+             // Second
 
-// const age = prompt('Введите ваш возраст');
-// const discount = age < 18 ? 10 : age <= 65 ? 20 : 30;
-// console.log(`Скидка ${discount}%`);
+const age = prompt('Введите ваш возраст');
+const discount = age < 18 ? 10 : age <= 65 ? 20 : 30;
+console.log(`Скидка ${discount}%`);
 
-// const age = prompt('Введите ваш возраст');
+const age = prompt('Введите ваш возраст');
 
-// let discount = 0;
+let discount = 0;
 
-// switch (true) {
-//     case age < 18:
-//         discount = 10;
-//         break
-//     case age <= 65 && age >= 18:
-//         discount = 20
-//         break
-//     default:
-//         discount = 30
-// }
+switch (true) {
+    case age < 18:
+        discount = 10;
+        break
+    case age <= 65 && age >= 18:
+        discount = 20
+        break
+    default:
+        discount = 30
+}
 
-// console.log(`Скидка ${discount}%`);
+console.log(`Скидка ${discount}%`);
 
 // ------------------------------------------------
 
-//               Three
+              // Three
 
-// const userName = prompt("Укажите ваше имя");
-// const password = prompt("Введите пароль");
+const userName = prompt("Укажите ваше имя");
+const password = prompt("Введите пароль");
 
-// switch (true) {
-//   case (userName === "admin" || userName === 'user') && password === "123456":
-//     alert("Доступ разрешен");
-//     break;
-//   default:
-//     alert("Доступ ограничен!");
-// }
+switch (true) {
+  case (userName === "admin" || userName === 'user') && password === "123456":
+    alert("Доступ разрешен");
+    break;
+  default:
+    alert("Доступ ограничен!");
+}
 
-//             // Four
+// ------------------------------------------------
 
-// const weight = prompt("Напишите вес посылки(в киллограмах)");
-// const deliveryType = prompt(
-//   "Выберите тип доставки / Стандарт / Экспресс / Премиум"
-// );
+            // Four
 
-// if (weight <= 0) {
-//   alert("Некорректный вес посылки");
-// } else if (
-//   deliveryType != "Стандарт" &&
-//   deliveryType != "Экспресс" &&
-//   deliveryType != "Премиум"
-// ) {
-//   alert("Неверный тип доставки");
-// }
+const weight = prompt("Напишите вес посылки(в киллограмах)");
+const deliveryType = prompt(
+  "Выберите тип доставки / Стандарт / Экспресс / Премиум"
+);
 
-// const price = weight < 1 ? 5 : weight <= 5 ? 10 : 15;
+if (weight <= 0) {
+  alert("Некорректный вес посылки");
+} else if (
+  deliveryType != "Стандарт" &&
+  deliveryType != "Экспресс" &&
+  deliveryType != "Премиум"
+) {
+  alert("Неверный тип доставки");
+}
 
-// let coefficient = 0;
+const price = weight < 1 ? 5 : weight <= 5 ? 10 : 15;
 
-// switch (true) {
-//   case deliveryType === "Стандарт":
-//     coefficient = 1;
-//     break;
-//   case deliveryType === "Экспресс":
-//     coefficient = 1.5;
-//     break;
-//   case deliveryType === "Премиум":
-//     coefficient = 2;
-//     break;
-// }
+let coefficient = 0;
 
-// const result = price * coefficient;
+switch (true) {
+  case deliveryType === "Стандарт":
+    coefficient = 1;
+    break;
+  case deliveryType === "Экспресс":
+    coefficient = 1.5;
+    break;
+  case deliveryType === "Премиум":
+    coefficient = 2;
+    break;
+}
 
-// if (result > 0) {
-//     alert(`Итоговая стоимость доставки: ${result}$`);
-// }
+const result = price * coefficient;
+
+if (result > 0) {
+    alert(`Итоговая стоимость доставки: ${result}$`);
+}
+
+
+// ---------------------------------------------------------
 
 
 
