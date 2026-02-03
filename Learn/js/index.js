ed20d37a6d65558c9f8c1bdac74d5224d4c7665a
QuoFN <116729072+QuoFN@users.noreply.github.com>
2026-02-03 16:43:22 +0300

    Add files via upload

--- Learn/js/index.js
+++ Learn/js/index.js
@@ -1,22 +1,134 @@
-const firstName = "Andrey";
-const lastName = " Hobin";
-const isStudent = true;
+// const a = +prompt("Введите первое число", 0);
+// const b = +prompt("Введите первое число", 0);
 
-const age = 20;
-const currentYear = 2026;
-const birthYear = currentYear - age;
-console.log(birthYear); 
+// const max = !isNaN(a) || !isNaN(b) ? (a > b ? a : b) : "Что то пошло не так";
 
-console.log(`Меня зовут ${firstName + lastName}, мне ${age} лет. Я ученик курса: ${isStudent}`);
+// // if (a > b) {
+// //     max = a;
+// // } else {
+// //     max = b;
+// // }
 
-let a = '123'
-let b = +'456'
-let c = Number('789')
-let d = Boolean(0)
-let e = Boolean (' ')
-let result = a + b + c + d + e;
+// console.log(max);
 
-// 123456789
-// 123456789falsetrue
+// const weather = "sun";
 
-console.log(result);
+// switch (weather) {
+//   case "rain":
+//     console.log("Остаюсь дома");
+//     break;
+//   case "sun":
+//     console.log("Иду гулять");
+//     break;
+//   default:
+//     console.log("Что то с погодой не так...");
+// }
+
+// const ADMIN = "admin";
+// const MODERATOR = "moderator";
+// const USER = "user";
+
+// const userRole = ADMIN;
+
+// switch (userRole) {
+//     case "admin":
+//     case "moderator":
+//         console.log('Добро пожаловать!');
+//         break
+//     default:
+//         console.log("Тебе сюда нельзя!");
+// }
+
+// ---------------------------------------------------------------------------------------------------------------
+
+            //   First
+
+// const number = prompt('Введите число');
+// const fixNumber = 2;
+
+// if (number % fixNumber) {
+//     alert('Число не четное');
+// } else {
+//     alert('Число четное');
+// }
+
+// ------------------------------------
+
+//              Second
+
+// const age = prompt('Введите ваш возраст');
+// const discount = age < 18 ? 10 : age <= 65 ? 20 : 30;
+// console.log(`Скидка ${discount}%`);
+
+// const age = prompt('Введите ваш возраст');
+
+// let discount = 0;
+
+// switch (true) {
+//     case age < 18:
+//         discount = 10;
+//         break
+//     case age <= 65 && age >= 18:
+//         discount = 20
+//         break
+//     default:
+//         discount = 30
+// }
+
+// console.log(`Скидка ${discount}%`);
+
+// ------------------------------------------------
+
+//               Three
+
+// const userName = prompt("Укажите ваше имя");
+// const password = prompt("Введите пароль");
+
+// switch (true) {
+//   case (userName === "admin" || userName === 'user') && password === "123456":
+//     alert("Доступ разрешен");
+//     break;
+//   default:
+//     alert("Доступ ограничен!");
+// }
+
+//             // Four
+
+// const weight = prompt("Напишите вес посылки(в киллограмах)");
+// const deliveryType = prompt(
+//   "Выберите тип доставки / Стандарт / Экспресс / Премиум"
+// );
+
+// if (weight <= 0) {
+//   alert("Некорректный вес посылки");
+// } else if (
+//   deliveryType != "Стандарт" &&
+//   deliveryType != "Экспресс" &&
+//   deliveryType != "Премиум"
+// ) {
+//   alert("Неверный тип доставки");
+// }
+
+// const price = weight < 1 ? 5 : weight <= 5 ? 10 : 15;
+
+// let coefficient = 0;
+
+// switch (true) {
+//   case deliveryType === "Стандарт":
+//     coefficient = 1;
+//     break;
+//   case deliveryType === "Экспресс":
+//     coefficient = 1.5;
+//     break;
+//   case deliveryType === "Премиум":
+//     coefficient = 2;
+//     break;
+// }
+
+// const result = price * coefficient;
+
+// if (result > 0) {
+//     alert(`Итоговая стоимость доставки: ${result}$`);
+// }
+
+
