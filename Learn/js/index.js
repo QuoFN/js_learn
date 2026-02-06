8fe4e81614395f31e0fbf3074f323ee83be15421
QuoFN <116729072+QuoFN@users.noreply.github.com>
2026-02-06 03:10:52 +0300

    Update index.js

--- Learn/js/index.js
+++ Learn/js/index.js
@@ -1,103 +1,33 @@
+//                First
 
-// ---------------------------------------------------------------------------------------------------------------
+const num = 20;
 
-              // First
-
-const number = prompt('Введите число');
-const fixNumber = 2;
-
-if (number % fixNumber) {
-    alert('Число не четное');
-} else {
-    alert('Число четное');
+for (let i = 1; i <= num; i++) {
+  if (i % 4 === 0) {
+    continue;
+  }
+  console.log(i);
 }
 
-// ------------------------------------
+//                Second
 
-             // Second
+let factorial = +prompt('Введите факториал числа: ', 1);
+let result = 1;
 
-const age = prompt('Введите ваш возраст');
-const discount = age < 18 ? 10 : age <= 65 ? 20 : 30;
-console.log(`Скидка ${discount}%`);
-
-// -------------------------------------------------
-
-const age = prompt('Введите ваш возраст');
-
-let discount = 0;
-
-switch (true) {
-    case age < 18:
-        discount = 10;
-        break
-    case age <= 65 && age >= 18:
-        discount = 20
-        break
-    default:
-        discount = 30
+for (let i = 1; i <= factorial; i++) {
+  result = result * i;
 }
 
-console.log(`Скидка ${discount}%`);
+console.log(`${factorial}! = ${result}`);
 
-// ------------------------------------------------
+//                Three
 
-              // Three
+for (let row = 0; row < 8; row++) {
+  let line = "";
 
-const userName = prompt("Укажите ваше имя");
-const password = prompt("Введите пароль");
+  for (let col = 0; col < 8; col++) {
+    line += (row + col) % 2 == 0 ? "⬛ " : "⬜ ";
+  }
 
-switch (true) {
-  case (userName === "admin" || userName === 'user') && password === "123456":
-    alert("Доступ разрешен");
-    break;
-  default:
-    alert("Доступ ограничен!");
+  console.log(line);
 }
-
-// ------------------------------------------------
-
-            // Four
-
-const weight = prompt("Напишите вес посылки(в киллограмах)");
-const deliveryType = prompt(
-  "Выберите тип доставки / Стандарт / Экспресс / Премиум"
-);
-
-if (weight <= 0) {
-  alert("Некорректный вес посылки");
-} else if (
-  deliveryType != "Стандарт" &&
-  deliveryType != "Экспресс" &&
-  deliveryType != "Премиум"
-) {
-  alert("Неверный тип доставки");
-}
-
-const price = weight < 1 ? 5 : weight <= 5 ? 10 : 15;
-
-let coefficient = 0;
-
-switch (true) {
-  case deliveryType === "Стандарт":
-    coefficient = 1;
-    break;
-  case deliveryType === "Экспресс":
-    coefficient = 1.5;
-    break;
-  case deliveryType === "Премиум":
-    coefficient = 2;
-    break;
-}
-
-const result = price * coefficient;
-
-if (result > 0) {
-    alert(`Итоговая стоимость доставки: ${result}$`);
-}
-
-
-// ---------------------------------------------------------
-
-
-
-
